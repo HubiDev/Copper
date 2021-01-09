755f7f1499a016a62c8b949949cec463031faa93
HubiDev <lukas.huber7@gmail.com>
2021-01-09 12:12:21 +0100

    Started adding code

---
+++ Sources/Copper/Graphics/Drawable.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Lukas on 09.01.21.
+//
+
+import Foundation
+
+
+protocol Drawable {
+    func draw() -> Void
+    
+    }
+}
---
+++ Sources/Copper/Graphics/Rectangle.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Lukas on 09.01.21.
+//
+
+import Foundation
+
+class Rectangle: Drawable {
+
+}
