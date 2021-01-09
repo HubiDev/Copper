88e8b05dc16a24e707420d77fa1a9dc0b264b801
HubiDev <lukas.huber7@gmail.com>
2021-01-09 16:01:07 +0100

    Initial Commit

---
+++ Copper/Copper.h
@@ -0,0 +1,18 @@
+//
+//  Copper.h
+//  Copper
+//
+//  Created by Lukas on 09.01.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Copper.
+FOUNDATION_EXPORT double CopperVersionNumber;
+
+//! Project version string for Copper.
+FOUNDATION_EXPORT const unsigned char CopperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Copper/PublicHeader.h>
+
+
