9194077691943d3811ab0047c59b7c2990d80869
HubiDev <lukas.huber7@gmail.com>
2021-05-15 21:32:33 +0200

    Preparations for controller integration

---
+++ Copper/Input/ControllerHandler.swift
@@ -0,0 +1,28 @@
+//
+//  ControllerHandler.swift
+//  Copper
+//
+//  Created by Lukas on 15.05.21.
+//
+
+import Foundation
+import GameController
+
+open class CPEControllerHandler {
+    
+    private var controllerInstance: GCController?
+    
+    public init() {
+        
+    }
+    
+    public func checkConnection() -> Bool {
+        
+        for controller in GCController.controllers() {
+            
+            print(controller.vendorName)
+            controllerInstance = controller
+        }
+        return true
+    }
+}
