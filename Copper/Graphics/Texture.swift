4878721bea8864ac00f2c126108f45c57cd4ce7b
HubiDev <lukas.huber7@gmail.com>
2021-11-06 13:33:35 +0100

    Wrote shaders for textured vertices

--- Copper/Graphics/Texture.swift
+++ Copper/Graphics/Texture.swift
@@ -8,12 +8,38 @@
 import Foundation
 import MetalKit
 
-open class CPETexture: CPEGameElement {
+open class CPETexture: CPEDrawable {
     
-    public init(_ device: MTLDevice, _ textureName: String, _ bundle: Bundle) {
+    let metalView: MTKView
+    let metalDevice: MTLDevice
+    let bundle: Bundle
+    let name: String
+    var metalTexture: MTLTexture?
+    var locationOffest: simd_float2
+    var renderPipelineState: MTLRenderPipelineState?
+    
+    public func draw(renderCommandEncoder: MTLRenderCommandEncoder) {
+        var transformParams = TransformParams(location: locationOffest)
+        
+        renderCommandEncoder.setRenderPipelineState(self.renderPipelineState!)
+        //renderCommandEncoder.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
+        //renderCommandEncoder.setVertexBytes(&transformParams, length: MemoryLayout<TransformParams>.stride, index: 1)
+        //renderCommandEncoder.drawPrimitives(type: .triangleStrip, vertexStart: 0, vertexCount: 6)
+    }
+    
+    
+    public init?(_ view: MTKView, _ device: MTLDevice, _ textureName: String, _ bundle: Bundle) {
+        self.metalView = view
         self.metalDevice = device
         self.name = textureName
         self.bundle = bundle
+        self.locationOffest = [0,0]
+        
+        do {
+            self.renderPipelineState = try CPETexture.buildRenderPipelineWithDevice(device: self.metalDevice, metalKitView: self.metalView)
+        } catch {
+            return nil;
+        }
     }
     
     public func update() {
@@ -31,11 +57,27 @@ open class CPETexture: CPEGameElement {
         
     }
     
-    let metalDevice: MTLDevice
-    let bundle: Bundle
-    let name: String
-    var metalTexture: MTLTexture?
-    
-    
-    
+    class func buildRenderPipelineWithDevice(device: MTLDevice,
+                                             metalKitView: MTKView) throws -> MTLRenderPipelineState? {
+        /// Build a render state pipeline object
+        
+        guard let bundle = Bundle(identifier: "HubiDev.Copper") else {
+            return nil
+        }
+        
+        let library = try? device.makeDefaultLibrary(bundle: bundle)
+        
+        let vertexFunction = library?.makeFunction(name: "textureVertexShader")
+        let fragmentFunction = library?.makeFunction(name: "textureFragmentShader")
+        
+        let pipelineDescriptor = MTLRenderPipelineDescriptor()
+        pipelineDescriptor.label = "RenderPipeline"
+        pipelineDescriptor.sampleCount = metalKitView.sampleCount
+        pipelineDescriptor.vertexFunction = vertexFunction
+        pipelineDescriptor.fragmentFunction = fragmentFunction
+        
+        pipelineDescriptor.colorAttachments[0].pixelFormat = metalKitView.colorPixelFormat
+        
+        return try device.makeRenderPipelineState(descriptor: pipelineDescriptor)
+    }
 }
--- Copper/Shaders/Shaders.metal
+++ Copper/Shaders/Shaders.metal
@@ -17,6 +17,11 @@ struct ShaderVertex{
     vector_float2 position;
 };
 
+struct TexturedShaderVertex{
+    vector_float2 textureCoordinate;
+    vector_float2 position;
+};
+
 struct TransformParams{
     vector_float2 position;
 };
@@ -25,6 +30,11 @@ struct TransformParams{
 struct VertexOut {
     float4 color;
     float4 pos [[position]];
+};
+
+struct TexturedVertexOut {
+    float2 textureCoordinate;
+    float4 position [[position]];
 };
 
 vertex VertexOut vertexShader(const device ShaderVertex* vertexArray [[buffer(0)]], const device TransformParams* transform, unsigned int vid [[vertex_id]])
@@ -39,11 +49,31 @@ vertex VertexOut vertexShader(const device ShaderVertex* vertexArray [[buffer(0)
     // Pass the already normalized screen-space coordinates to the rasterizer
     out.pos = float4(in.position.x + transform->position.x, in.position.y + transform->position.y, 0, 1);
     
-    
     return out;
 }
 
 fragment float4 fragmentShader(VertexOut interpolated [[stage_in]])
 {
     return interpolated.color;
+}
+
+vertex TexturedVertexOut textureVertexShader(const device TexturedShaderVertex* vertexArray [[buffer(0)]], const device TransformParams* transform, unsigned int vid [[vertex_id]])
+{
+    TexturedVertexOut out;
+    // Get the data for the current vertex.
+    auto in = vertexArray[vid];
+    
+    out.position = float4(in.position.x + transform->position.x, in.position.y + transform->position.y, 0, 1);
+    out.textureCoordinate = vertexArray[vid].textureCoordinate;
+    
+    return out;
+}
+
+fragment float4 textureFragmentShader(TexturedVertexOut in [[stage_in]], texture2d<half> colorTexture [[texture(0)]])
+{
+    constexpr sampler textureSampler(mag_filter::linear, min_filter::linear);
+    
+    const auto colorSample = colorTexture.sample(textureSampler, in.textureCoordinate);
+    
+    return float4(colorSample);
 }
